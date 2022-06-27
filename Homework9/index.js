1dfaf1d0ff77a35656c117327ca9074dbea7da90
Maria <105918105+Maria-Bondar@users.noreply.github.com>
2022-06-27 11:26:18 +0300

    Homework 10

---
+++ Homework9/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  //task 1
+  $("a").attr("target", "_blank");
+
+  //task 2
+  $("h2").find(".head");
+  $(".head").css("background-color", "lightgreen");
+  $(".head").find(".inner");
+  $(".inner").css("font-size", "35px");
+
+  //task 3
+
+  $("h3").each((index, element) => element.before($("div")[index]));
+
+  //task 4
+
+  let countCheckbox = 0;
+  $(".chbox").change(function () {
+    if (this.checked == true) {
+      countCheckbox += 1;
+    }
+    if (this.checked == false) {
+      countCheckbox -= 1;
+    }
+    if (countCheckbox > 3) {
+      $(".chbox").attr("disabled", true);
+    }
+  });
+});
